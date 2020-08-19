1eef9ae1d45687a7b5f1a25bd01b47acd0fe5fb8
bin <liubin@qianmi.com>
2020-08-19 17:17:22 +0800

    格式化代码及备注添加

--- resumes/liubin.js
+++ resumes/liubin.js
@@ -29,8 +29,8 @@
  * 
  * 
  * 
- * 
- * wxQrcode:微信二维码
+ * headerImg:头像地址
+ * wxQrcode:微信二维码地址
  */
 
 module.exports = {
@@ -49,47 +49,47 @@ module.exports = {
         '2. 责任心强，适应能力强，意志坚毅不拔，执行力强，善于沟通；',
         '3. 勇于迎接新的挑战，具备很强的事业心和团队协作的精神，愿接受一切工作的考验，相信自己的加入会给企业或是公司创造更高的价值。'
     ],
-    workList:[{
-        companyName:'千米网',
-        department:'新零售事业部',
-        job:'前端工程师',
-        times:'2019.8-至今',
-        introduce:[
-          '1. 开发1000.com',
-          '2. 开发千米云采购小程序'
+    workList: [{
+        companyName: '千米网',
+        department: '新零售事业部',
+        job: '前端工程师',
+        times: '2019.8-至今',
+        introduce: [
+            '1. 开发1000.com',
+            '2. 开发千米云采购小程序'
         ]
     }],
-    skillList:[{
-        skillName:'HTML/HTML5',
-        progress:'90%',
-        color:'blue'
-      },{
-        skillName:'React',
-        progress:'90%',
-        color:'olive'
-      },{
-        skillName:'CSS/CSS3',
-        progress:'87%',
-        color:'orange'
-      },{
-        skillName:'JavaScript',
-        progress:'86%',
-        color:'green'
-      },{
-        skillName:'JQuery',
-        progress:'84%',
-        color:'yellow'
-      },{
-        skillName:'Vue',
-        progress:'80%',
-        color:'cyan'
-      },{
-        skillName:'Node',
-        progress:'79%',
-        color:'pink'
-      }],
+    skillList: [{
+        skillName: 'HTML/HTML5',
+        progress: '90%',
+        color: 'blue'
+    }, {
+        skillName: 'React',
+        progress: '90%',
+        color: 'olive'
+    }, {
+        skillName: 'CSS/CSS3',
+        progress: '87%',
+        color: 'orange'
+    }, {
+        skillName: 'JavaScript',
+        progress: '86%',
+        color: 'green'
+    }, {
+        skillName: 'JQuery',
+        progress: '84%',
+        color: 'yellow'
+    }, {
+        skillName: 'Vue',
+        progress: '80%',
+        color: 'cyan'
+    }, {
+        skillName: 'Node',
+        progress: '79%',
+        color: 'pink'
+    }],
 
-    headerImg:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1597838366369&di=d0de0a88ac3e8823755868dc5141da92&imgtype=0&src=http%3A%2F%2Fss.csdn.net%2Fp%3Fhttps%3A%2F%2Fmmbiz.qpic.cn%2Fmmbiz_png%2FctwVEjeYQ2ic9QVvaUJoxwiaL1eFJ7iaKgcN5baEyQ5N8IpKL5kpCcExGy7Sn9kqYIawYdIAibtPUMuAm7zafXUwTQ%2F0%3Fwx_fmt%3Dpng',
+    headerImg: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1597838366369&di=d0de0a88ac3e8823755868dc5141da92&imgtype=0&src=http%3A%2F%2Fss.csdn.net%2Fp%3Fhttps%3A%2F%2Fmmbiz.qpic.cn%2Fmmbiz_png%2FctwVEjeYQ2ic9QVvaUJoxwiaL1eFJ7iaKgcN5baEyQ5N8IpKL5kpCcExGy7Sn9kqYIawYdIAibtPUMuAm7zafXUwTQ%2F0%3Fwx_fmt%3Dpng',
 
     wxQrcode: 'http://m.qpic.cn/psc?/V13CtUlR1Y82A2/bqQfVz5yrrGYSXMvKr.cqbPf*1GeoYXhHPFSBdl9p2eKQm*bJ9ityyAIhzn5s*scdYya0C4bPMZx0KJqR2TYdonJlCeUMoBw7hguz8AVZyk!/b&bo=OAR2BQAAAAADB20!&rf=viewer_4'
 }
